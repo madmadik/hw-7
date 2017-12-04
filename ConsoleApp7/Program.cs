28e940c7ed2009a7c24059d0e00026e2670c8629
Курмангалиев Мадияр Амиргазыулы <@corp.itstep>
2017-12-04 11:46:21 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeOfArray maas = new RangeOfArray();
+            maas.get();
+            maas.Min = int.Parse(Console.ReadLine());
+            maas.Max = int.Parse(Console.ReadLine());
+            maas[-5] = 5;
+            maas[-4] = 4;
+            maas[-3] = 6;
+            maas[-2] = 7;
+
+            Console.WriteLine(maas[-5]);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp7/RangeOfArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class RangeOfArray
+    {
+        int[] mas;
+        static int size;
+        public void get()
+        {
+            int.TryParse(Console.ReadLine(), out size);
+            mas = new int[size];
+        }
+
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int this[int index] 
+        {
+            get
+            {
+                return mas[index - Min];
+            }
+            set
+            {
+                int u = index - Min;
+                mas[u] = value;
+            }
+        }
+
+
+
+    }
+}
